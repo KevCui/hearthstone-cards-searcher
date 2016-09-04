b2d64f3e5a1e6fe2b2906b7c007b8efcddbd74d7
Kevin Cui <krazycavin@gmail.com>
2016-09-04 17:50:10 +0200

    add filter by class

--- hsSearch.py
+++ hsSearch.py
@@ -19,9 +19,37 @@ def getOptionValue(soup):
 def verifyValue(target, attribute):
     "verify attribute exists in target"
     if attribute in target.keys():
+        if attribute == "set":
+            logging.debug(target)
         return str(target[attribute])
     else:
         return ''
+
+def isCardFiltered(card, filter):
+    "filter card by filters"
+    if filter:
+        for f in filter:
+            if f in card.keys():
+                logging.debug(card)
+                logging.debug(card[f])
+                logging.debug(type(card[f]))
+                logging.debug(filter[f])
+                logging.debug(type(filter[f]))
+                if str(card[f]) != str(filter[f]):
+                    return True
+            else:
+                return True
+    return False
+
+def setNoneValue(data, filter):
+    "set None value in data ilterif it doesn't exist according to filter list"
+    tmp = []
+    for card in data:
+        for f in filter:
+            if f not in card.keys():
+                card[f] = ''
+        tmp.append(card)
+    return tmp
 
 # Assert beautifulsoup is installed
 try:
@@ -43,6 +71,7 @@ except ImportError:
 parser = argparse.ArgumentParser()
 parser.add_argument('name', nargs='*', help='search text')
 parser.add_argument('-s', '--sort', choices=['cost', 'popularity'], dest="sortby", help='sort cards by cost or popularity. Default sort by cost')
+parser.add_argument('-c', '--class', choices=['neutral', 'warrior', 'priest',  'hunter', 'rogue', 'paladin', 'shaman', 'mage', 'warlock', 'druid'], dest='flclass', help='filter by class')
 parser.add_argument('-d', '--debug',  action='store_true', dest="debug", help='active debug log')
 args = parser.parse_args()
 
@@ -62,6 +91,29 @@ logging.debug('\n' + url)
 sortby = "cost" if args.sortby is None else args.sortby
 logging.debug('sortby: ' + sortby)
 
+# print table head title
+printTitle = ['Name', 'Cost', 'Attack', 'Life', 'Description', 'classes', 'Set', 'Race', 'Rarity', 'Porularity']
+
+# create classes dict for class map
+classes = {
+    '':     'Neutral',
+    '1':    'Warrior',
+    '2':    'Priest',
+    '3':    'Hunter',
+    '4':    'Rogue',
+    '5':    'Paladin',
+    '7':    'Shaman',
+    '8':    'Mage',
+    '9':    'Warlock',
+    '11':   'Druid'
+}
+
+# create filters
+filters = {}
+if args.flclass is not None:
+    filters['classs'] = list(classes.keys())[list(classes.values()).index(args.flclass[0].upper() + args.flclass[1:])]
+logging.debug('Filters:\n' + str(filters))
+
 # create http request
 req = urllib.request.Request(url)
 req.add_header('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11')
@@ -78,13 +130,14 @@ for filter in soup.find('table', class_='filter-table').find_all('tr'):
         sets = getOptionValue(filter)
         logging.debug('Sets:\n' + str(sets))
 
-# create rarities dict for rarity map
+# create rarities dict for quality map
 rarities = getOptionValue(soup.find('select', class_='rightselect'))
 logging.debug('Rarities:\n' + str(rarities))
 
 # fetch data from script and generate cards json
 for script in soup.find_all('script'):
     if 'hearthstoneCards' in script.text:
+        logging.debug('--raw--')
         p = re.compile('var hearthstoneCards = \[{(.*?)\}]')
         raw = '[{' + p.search(script.text).group(1) + '}]'
         raw = raw.replace('popularity:', '"popularity":') # <- spent 1H to fix broken json! Dirty data...
@@ -92,28 +145,40 @@ for script in soup.find_all('script'):
         logging.debug('--raw--')
         logging.debug(raw)
 
-        rawjson = sorted(json.loads(raw), key=lambda k: int(k[sortby]), reverse=True)
+        rawjson= sorted(json.loads(raw), key=lambda k: int(k[sortby]), reverse=True)
         logging.debug('--json--')
+        logging.debug(rawjson)
+        rawlist = setNoneValue(rawjson, filters)
+        logging.debug('--list--')
         logging.debug(rawjson)
 
         break
+try:
+    rawjson
+except NameError:
+    print("Not match any Hearthstone cards.")
+    sys.exit(1)
 
 # Prepared cards list for print
 printCards = []
 for card in rawjson:
-    printCards.append([
-        verifyValue(card, 'name'),
-        verifyValue(card, 'cost'),
-        verifyValue(card, 'attack'),
-        verifyValue(card, 'health'),
-        verifyValue(card, 'description'),
-        sets[verifyValue(card, 'set')],
-        races[verifyValue(card, 'race')],
-        rarities[verifyValue(card, 'quality')], # why quality??
-        verifyValue(card, 'popularity')
-    ])
+    try:
+        if not isCardFiltered(card, filters):
+            printCards.append([
+                verifyValue(card, 'name'),
+                verifyValue(card, 'cost'),
+                verifyValue(card, 'attack'),
+                verifyValue(card, 'health'),
+                verifyValue(card, 'description'),
+                classes[verifyValue(card, 'classs')], # why classs??
+                sets[verifyValue(card, 'set')],
+                races[verifyValue(card, 'race')],
+                rarities[verifyValue(card, 'quality')], # why quality??
+                verifyValue(card, 'popularity')
+            ])
+    except:
+        pass
 logging.debug(printCards)
 
 # Print cards table
-print(tabulate(printCards, ['Name', 'Cost', 'Attack', 'Life', 'Description', 'Set', 'Race', 'Rarity', 'Porularity'], tablefmt="grid"))
-sys.exit(1)
+print(tabulate(printCards, printTitle, tablefmt="grid"))
